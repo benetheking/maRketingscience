20a57364c20bf5be167374fc53788367e962b5d0
Benedikt <35131697+benetheking@users.noreply.github.com>
2021-05-18 18:12:03 +0200

    Add files via upload

--- R/set_dummy.R
+++ R/set_dummy.R
@@ -1,30 +1,29 @@
 #' A function for rapid creation of an outlier catch dummy variable
 #'
 #' This function allows you to quickly create a dummy variable for controlling outliers
-#' @param modell Input data. Just name the model you want to create a dummy for
+#' @param model Input data. It's just name the model that you want the dummy for.
+#' @param sd Controls the cut-off value for the dummy variable that is being generated. Can be a non-integer like 1.5. Defaults to 1.
 #' @keywords create dummy variable
 #' @export
-#' @examples set_dummy(my_default_model) ## use the object assignment operator ' <- ' to include the dummy into your dataset
+#' @examples set_dummy(model1) ## use the object assignment operator ' <- ' to include the dummy into your dataset
 #' set_dummy()
 
 ## DUMMY MUSS DU REINBALLERN
-set_dummy <- function(modell) {
-  calc <- modell %>%
+set_dummy <- function(model = model1, sd = 1) {
+  calc <- model %>%
     `$`(residuals) %>% sd()
 
-  dummel <- modell %>%
+  dummel <- model %>%
     `$`(residuals) %>%
-    map_dbl(., ~ifelse(. < -calc, -1,
-                       ifelse(. > calc, 1, 0)))
+    map_dbl(., ~ifelse(. < -(sd*calc), -1,
+                       ifelse(. > (sd*calc), 1, 0)))
   #  ## das sind die daten:
   #  dati <- hans %>%
   #    `$`(call) %>%
   #    `$`(data) %>%
   #    eval(.)
-
   ## schreibe Dummy in die Daten rein
   #  dummel -> dati$dummel
-
   #  return(dati$dummel)
   #}
 
